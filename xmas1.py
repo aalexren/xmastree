066635cac52b917be377e427fbea80a6bd18a62f
Artyom Bratov <chernitca@ya.ru>
2019-01-02 00:41:59 +0300

    Add files via upload

--- xmas1.py
+++ xmas1.py
@@ -43,33 +43,35 @@ curses.use_default_colors()
 #Добавляем цвета в палитру (цвета берутся из цветовой схемы терминала)
 for i in range(0, 255): 
   curses.init_pair(i + 1, i, -1)
+try:
+  while True:
+    temp = 1
+    xmas(stdscr, width//3, 0)  # текст figlet
+    sign(stdscr, width//3 + 4, height//2 + 3) # подпись
+    for level in range(0, height//2):          # проще говоря, это высота ёлки
+      for _ in range(0, (width//3-temp)//2 + height//(height//10)//2):
+        x += 1
+      for _ in range(0, temp):
+        stdscr.addstr(y, x, '*', curses.color_pair(random.randint(20, 230)))
+        x += 1
+      for _ in range(0, (width//3-temp)//2 + height//(height//10)//2):
+        x += 1
+      temp += 2
+      y += 1
+      x = 0
 
-while True:
-  temp = 1
-  xmas(stdscr, width//3, 0)  # текст figlet
-  sign(stdscr, width//3 + 4, height//2 + 3) # подпись
-  for level in range(0, height//2):          # проще говоря, это высота ёлки
-    for _ in range(0, (width//3-temp)//2 + height//(height//10)//2):
-      x += 1
-    for _ in range(0, temp):
-      stdscr.addstr(y, x, '*', curses.color_pair(random.randint(20, 230)))
-      x += 1
-    for _ in range(0, (width//3-temp)//2 + height//(height//10)//2):
-      x += 1
-    temp += 2
-    y += 1
-    x = 0
-
-  for i in range(0, long): # высота ножки
-    for space in range(0, (width//3-foot)//2 + height//(height//10)//2):
-      x += 1
-    for stick in range(0, foot): # ну с шириной ножки можно играться
-      stdscr.addstr(y, x, '|')
-      x += 1
-    for space in range(0, (width//3-foot)//2 + height//(height//10)//2):
-      x += 1
-    y += 1
-    x = 0
-  stdscr.refresh()
-  x, y = 0, 0
-  time.sleep(0.5)
+    for i in range(0, long): # высота ножки
+      for space in range(0, (width//3-foot)//2 + height//(height//10)//2):
+        x += 1
+      for stick in range(0, foot): # ну с шириной ножки можно играться
+        stdscr.addstr(y, x, '|')
+        x += 1
+      for space in range(0, (width//3-foot)//2 + height//(height//10)//2):
+        x += 1
+      y += 1
+      x = 0
+    stdscr.refresh()
+    x, y = 0, 0
+    time.sleep(0.5)
+except:
+  curses.endwin()
